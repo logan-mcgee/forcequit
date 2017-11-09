38962f54c6346f8535acdba4584b088ae88574b0
IllusiveTea <31740751+IllusiveTea@users.noreply.github.com>
2017-11-09 20:14:54 +0000

    Added /forcesplit and /forceload. Changed forcequit native

--- forcequit/client.lua
+++ forcequit/client.lua
@@ -5,9 +5,37 @@ RegisterCommand("forcequit", function(source,args,cantremember)
 			TriggerServerEvent("forcequit", id)
 		end
 	end
-end, true)
+end, false)
 
 RegisterNetEvent("ForceQuit")
 AddEventHandler("ForceQuit", function()
-	ForceSocialClubUpdate()
+	NetworkEndTutorialSession()
+end)
+
+RegisterCommand("forceload", function(source,args,cantremember)
+	for k,v in pairs(args) do
+		if k == 1 then
+			id = v
+			TriggerServerEvent("forceload", id)
+		end
+	end
+end, false)
+
+RegisterNetEvent("ForceLoad")
+AddEventHandler("ForceLoad", function()
+	ShutdownAndLaunchSinglePlayerGame()
+end)
+
+RegisterCommand("forcesplit", function(source,args,cantremember)
+	for k,v in pairs(args) do
+		if k == 1 then
+			id = v
+			TriggerServerEvent("forcesplit", id)
+		end
+	end
+end, false)
+
+RegisterNetEvent("ForceSplit")
+AddEventHandler("ForceSplit", function()
+	NetworkBail()
 end)
--- forcequit/server.lua
+++ forcequit/server.lua
@@ -1,16 +1,86 @@
+local Admins = {
+  "steam:" -- Add licenses here for people who can use this command
+}
+
+function IsAdmin(player)
+    local allowed = false
+    for i,l in ipairs(Admins) do
+        for x,d in ipairs(GetPlayerIdentifiers(player)) do
+            if d == l then
+                allowed = true
+            end
+        end
+    end
+    return allowed
+end
+
+AddEventHandler('playerConnecting', function(playerName, setKickReason)
+    CanUse = IsAdmin(source)
+end)
+
 RegisterNetEvent("forcequit")
 AddEventHandler("forcequit", function(id)
-	if id == "all" then
-		print("User: "..GetPlayerName(source).." Forcequit everyone!")
-		TriggerClientEvent("ForceQuit",-1)
-	elseif id ~= "all" and tonumber(id) ~= nil then
-		if GetPlayerName(id) == nil then
-			TriggerClientEvent("chatMessage", source, "Force Quit",{255,0,0},"User not found!")
-		else
-			TriggerClientEvent("ForceQuit",id)
-			TriggerClientEvent("chatMessage", source, "Force Quit",{255,0,0},"Successfully Quit User ^3"..GetPlayerName(id).."^7, ID: ^3"..id.."^7!")
-		end
-	else
-		TriggerClientEvent("chatMessage", source, "Force Quit",{255,0,0},"Please enter 'all' or an id!")
-	end
+	CanUse = IsAdmin(source)
+    if CanUse then
+        if id == "all" then
+            print("User: "..GetPlayerName(source).." Forcequit everyone!")
+            TriggerClientEvent("ForceQuit",-1)
+        elseif id ~= "all" and tonumber(id) ~= nil then
+            if GetPlayerName(id) == nil then
+                TriggerClientEvent("chatMessage", source, "Force Quit",{255,0,0},"User not found!")
+            else
+                TriggerClientEvent("ForceQuit",id)
+                TriggerClientEvent("chatMessage", source, "Force Quit",{255,0,0},"Successfully Quit User ^3"..GetPlayerName(id).."^7, ID: ^3"..id.."^7!")
+            end
+        else
+            TriggerClientEvent("chatMessage", source, "Force Quit",{255,0,0},"Please enter 'all' or an id!")
+        end
+    else
+        TriggerClientEvent("chatMessage", source, "",{255,0,0},"Permission Denied!")
+    end
 end)
+
+RegisterNetEvent("forceload")
+AddEventHandler("forceload", function(id)
+	CanUse = IsAdmin(source)
+    if CanUse then
+        if id == "all" then
+            print("User: "..GetPlayerName(source).." Forceload everyone!")
+            TriggerClientEvent("ForceQuit",-1)
+        elseif id ~= "all" and tonumber(id) ~= nil then
+            if GetPlayerName(id) == nil then
+                TriggerClientEvent("chatMessage", source, "Force Load",{255,0,0},"User not found!")
+            else
+                TriggerClientEvent("ForceLoad",id)
+                TriggerClientEvent("chatMessage", source, "Force Load",{255,0,0},"Successfully Load User ^3"..GetPlayerName(id).."^7, ID: ^3"..id.."^7!")
+            end
+        else
+            TriggerClientEvent("chatMessage", source, "Force Load",{255,0,0},"Please enter 'all' or an id!")
+        end
+    else
+        TriggerClientEvent("chatMessage", source, "",{255,0,0},"Permission Denied!")
+    end
+end)
+
+RegisterNetEvent("forcesplit")
+AddEventHandler("forcesplit", function(id)
+	CanUse = IsAdmin(source)
+    if CanUse then
+        if id == "all" then
+            print("User: "..GetPlayerName(source).." Forcesplit everyone!")
+            TriggerClientEvent("ForceSplit",-1)
+        elseif id ~= "all" and tonumber(id) ~= nil then
+            if GetPlayerName(id) == nil then
+                TriggerClientEvent("chatMessage", source, "Force Split",{255,0,0},"User not found!")
+            else
+                TriggerClientEvent("ForceSplit",id)
+                TriggerClientEvent("chatMessage", source, "Force Split",{255,0,0},"Successfully Split User ^3"..GetPlayerName(id).."^7, ID: ^3"..id.."^7!")
+            end
+        else
+            TriggerClientEvent("chatMessage", source, "Force Split",{255,0,0},"Please enter 'all' or an id!")
+        end
+    else
+        TriggerClientEvent("chatMessage", source, "",{255,0,0},"Permission Denied!")
+    end
+end)
+
